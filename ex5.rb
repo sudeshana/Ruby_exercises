d2498ec6a68ddd1d4d9f3cc86571a61f6215fe8f
Sudeshana Ariyasena <dgpradeepal@gmail.com>
2017-09-05 10:16:29 +0530

    Completed two exercises

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Sudeshana Ariyasena'
+my_age = 27
+my_height = 67
+my_weight = 160
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = "Black"
+
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+puts "If I add #{my_age} , #{my_height} and #{my_weight} I get #{my_age + my_height + my_weight}."
